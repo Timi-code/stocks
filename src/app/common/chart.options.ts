624c503867ac1680a9daf7ca4cf950460b2a681f
汤成立 <tangchengli@cloudwalk.cn>
2020-01-11 22:42:13 +0800

    图表默认选项

---
+++ src/app/common/chart.options.ts
@@ -0,0 +1,18 @@
+export const ChartDefaultOptions = {
+  time: {
+    useUTC: false
+  },
+  rangeSelector: {
+    enabled: false
+  },
+  navigator: {
+    enabled: false
+  },
+  scrollbar: {
+    enabled: false
+  },
+  tooltip: {
+    shared: true,
+    split: false
+  }
+};
